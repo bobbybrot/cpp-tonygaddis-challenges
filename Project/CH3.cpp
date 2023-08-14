756de3b2195036baba726b2b06948b48a6110ca8
bobbybrot <bobby.brot@hotmail.co.uk>
2023-08-14 21:44:54 +0100

    Commit of CH3.cpp
    Taken from master code (before changes made to make it a template for VS projects).

---
+++ Project/CH3.cpp
@@ -0,0 +1,15 @@
+/*
+* Chapter 3: Expressions and Interactivity
+* Book Pages: 144 - 150
+* 
+* NOTE: Each programming challenge are separated into their own functions.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello world\n";
+	return 0;
+}
