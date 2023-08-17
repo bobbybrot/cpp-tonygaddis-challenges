f6e00c0aa03844c7565742b42c7d19ab95d12149
bobbybrot <bobby.brot@hotmail.co.uk>
2023-08-17 22:36:27 +0100

    Challenge 1
    - Committed code for first challenge.
    - Minor adjustment to file header comments

--- Project/CH3.cpp
+++ Project/CH3.cpp
@@ -2,14 +2,39 @@
 * Chapter 3: Expressions and Interactivity
 * Book Pages: 144 - 150
 * 
-* NOTE: Each programming challenge are separated into their own functions.
+* NOTE: Each programming challenge are separated into their own functions:
+* 
+* XXX_NAME
+* 
+* XXX  = Challenge Number (matching number in book)
+* NAME = Name of challenge (matching challenge name from book).
 */
 
 #include<iostream>
 using namespace std;
 
+/****************** FUNCTION DEFINITIONS ******************/
+void One_Miles_Per_Gallon(void)
+{
+	float max_gallons_supported, max_miles_full_tank;
+
+	//Ask and read max tank gallons
+	cout << "Enter maximum amount of gallons the car tank supports: ";
+	cin >> max_gallons_supported;
+
+	//Ask and read max full tank miles
+	cout << "Enter max miles you can drive on a full tank: ";
+	cin >> max_miles_full_tank;
+
+	//Calculate and display MPG based on given information
+	//MPG = Miles_driven / gallons_used
+	cout << "MPG: " << (max_miles_full_tank / max_gallons_supported) << "MPG" << endl;
+}
+
+/****************** MAIN ******************/
 int main()
 {
-	cout << "Hello world\n";
+	One_Miles_Per_Gallon();
+
 	return 0;
 }
