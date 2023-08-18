cbcd0751c532c3ff659dff3022ddd94eb37646d4
bobbybrot <bobby.brot@hotmail.co.uk>
2023-08-18 20:52:16 +0100

    Challenge 2
    CH3.cpp
        - Commited challenge attempt for Two_Stadium_Seating.
        - Added iomanip for file, as required to access IO manipulation functions for this
          and future challenges

--- Project/CH3.cpp
+++ Project/CH3.cpp
@@ -11,6 +11,7 @@
 */
 
 #include<iostream>
+#include<iomanip>
 using namespace std;
 
 /****************** FUNCTION DEFINITIONS ******************/
@@ -31,10 +32,27 @@ void One_Miles_Per_Gallon(void)
 	cout << "MPG: " << (max_miles_full_tank / max_gallons_supported) << "MPG" << endl;
 }
 
+void Two_Stadium_Seating(void)
+{
+	//Ask for Class A,B,C how many seats sold.
+	float seats_sold_a, seats_sold_b, seats_sold_c, total_sales_income;
+	const float SEATS_A_PRICE = 15; //$15
+	const float SEATS_B_PRICE = 12; //$12
+	const float SEATS_C_PRICE =  9;  //$9
+
+	cout << "Pleaes enter (separated by spaces) number of tickets sold for A,B and C: " << endl;
+	cin >> seats_sold_a >> seats_sold_b >> seats_sold_c;
+
+	total_sales_income = (seats_sold_a * SEATS_A_PRICE) + (seats_sold_b * SEATS_B_PRICE) + (seats_sold_c * SEATS_C_PRICE);
+
+	cout << setprecision(2) << fixed << showpoint;
+	cout << "Total Income: $" << total_sales_income << endl;
+}
+
 /****************** MAIN ******************/
 int main()
 {
-	One_Miles_Per_Gallon();
-
+	//One_Miles_Per_Gallon();
+	//Two_Stadium_Seating();
 	return 0;
 }
